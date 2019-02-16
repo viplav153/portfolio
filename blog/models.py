4102db4c537a832a5e414a15b9d0c79edc62111c
viplav153 <viplav153@gmail.com>
2019-02-16 12:32:16 +0530

    commit all

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title= models.CharField(max_length=200)
+    date=models.DateTimeField()
+    text=models.TextField()
+    image =models.ImageField(upload_to="images/")
+    
+    
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +118,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR,'portfolio/static/')
+
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 #http://localhost:8000/media/images/fifa_ybaMeJZ.png
