cc7a07777ba3aaf563ce83504dc751117be7df23
viplav153 <viplav153@gmail.com>
2019-02-22 22:00:27 +0530

    added something

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField()
-    pub_date = models.DateTimeField()
+    text = models.TextField()
+    date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
 
     def summary(self):
-        return self.body
+        return self.text
 
     def pub_date_pretty(self):
-        return self.pub_date
+        return self.date
 
     
 
